300d0b7a52fb5d5887de552bdb982e63e40c95c4
Rituraj <rutu2930@gmail.com>
2026-02-28 15:30:12 +0530

    some change in frontend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -18,10 +18,13 @@ dotenv.config();
 
 const app: Application = express();
 const PORT: number = Number(process.env.PORT) || 5000;
-
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5174";
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: FRONTEND_URL,
+  credentials: true
+}));
 app.use(express.json()); 
 
 app.use(
